861a9da0976829c124e404353c4ead2205c5bfb1
spaceaffairs <spaceaffairs.iasa@gmail.com>
2023-01-24 17:26:08 +0100

    ajusted layout to norbertpape.github.io

---
+++ menu.js
@@ -0,0 +1,45 @@
+﻿document.write('<div id="myNav" class="overlay">\
+      <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>\
+      <div class="overlay-content">\
+        <a href="index.html">Home</a>\
+        <a href="news.html">News</a>\
+        <a onclick="openNav2();closeNav()">Archive</a>\
+        <a href="about.html">About</a>\
+        <a href="contact.html">Contact</a>\
+      </div>\
+    </div>\
+    <div id="myNav2" class="overlay">\
+      <a href="javascript:void(0)" class="closebtn" onclick="closeNav2()">&times;</a>\
+      <div class="overlay-content">\
+            <h1> Selected works </h1>\
+            <a href="digital.html">Digital</a>\
+            <a href="O.html">O</a>\
+         </div>\
+    </div>\
+    <span style="font-size:30px;color:white;cursor:pointer;z-index:99" onclick="openNav()">&#9776; menu</span>\
+    <script>\
+            function openNav() {\
+              document.getElementById("myNav").style.width = "100%";\
+            }\
+            function closeNav() {\
+              document.getElementById("myNav").style.width = "0%";\
+            }        \
+            function openNav2() {\
+              document.getElementById("myNav2").style.width = "100%";\
+            }\
+            function closeNav2() {\
+              document.getElementById("myNav2").style.width = "0%";\
+            }\
+            function openPage(pageName,elmnt) {\
+            var tabcontent;\
+            tabcontent = document.getElementsByClassName("tabcontent");\
+            for (var i = 0; i < tabcontent.length; i++) {\
+            tabcontent[i].style.display = "none";\
+            }\
+            document.getElementById(pageName).style.display = "block";\
+            elmnt.style.backgroundColor = "";\
+        }\
+        </script>\
+');
+
+//  <a href="schwerelos.html">Schwerelos</a>\
