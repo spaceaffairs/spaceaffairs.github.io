2f9396eba1c3a53380289774b7b5682f7e28159f
spaceaffairs <spaceaffairs.iasa@gmail.com>
2023-01-24 19:14:50 +0100

    header correction

--- header.js
+++ header.js
@@ -1,9 +1,9 @@
 ﻿document.write('<head>\
     <meta charset="utf-8">\
-    <title>Norbert Pape</title>\
+    <title>I.A.S.A.</title>\
     <link rel="stylesheet" href="styles/style.css" >\
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">\
     <meta name="viewport" content="width=device-width, initial-scale=1.0">\
-    <meta name="description" content="Personal website of Norbert Pape">\
-    <meta name="keywords" content="dance, art, choreography, VR, performance, augmented reality, contemporary art, virtual reality, unity, quest, oculus, pedagogy, self-organisation" />\
+    <meta name="description" content="Official Website of the International Agency for Space Affairs">\
+    <meta name="keywords" content="dance, art, choreography, VR, performance, augmented reality, contemporary art, virtual reality, unity, quest, oculus, pedagogy, self-organisation, math, mathematics" />\
   </head>');
