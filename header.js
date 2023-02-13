7144d2bdf0fa9d085881f35f01e501406f86e3f1
spaceaffairs <spaceaffairs.iasa@gmail.com>
2023-02-13 15:59:49 +0100

    added O Nr2 Frankfurt
    pictures + webpage

--- header.js
+++ header.js
@@ -3,6 +3,7 @@
     <title>I.A.S.A.</title>\
     <link rel="stylesheet" href="styles/style.css" >\
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">\
+    <link rel="canonical" href="https://spaceaffairs.github.io/" />\
     <meta name="viewport" content="width=device-width, initial-scale=1.0">\
     <meta name="description" content="Official Website of the International Agency for Space Affairs">\
     <meta name="keywords" content="dance, art, choreography, VR, performance, augmented reality, contemporary art, virtual reality, unity, quest, oculus, pedagogy, self-organisation, math, mathematics" />\
